19b2779f319a63dfbc188448de5cdf728139b0b2
AnaV-PM <anamaria.valdez@gmail.com>
2023-04-08 16:10:33 -0700

    programming-recursion-anav

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,11 +3,11 @@
  * _print_rev_recursion - Reverse the string
  * @s: string
  */
-
 void _print_rev_recursion(char *s)
 {
 	if (*s)
+	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
-
-}
+	}
+	}
