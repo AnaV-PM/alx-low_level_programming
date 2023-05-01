6c4b29937e949cc7c95fdba03ed4f4023b2b31cb
AnaV-PM <anamaria.valdez@gmail.com>
2023-05-01 06:23:08 -0700

    programming-static-libraries

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -1,20 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
- */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+ * @c: The character to print
+*/
 
-	return (0);
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
