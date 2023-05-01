d14979117cb5ebb62a7f03ce596741653056b51e
AnaV-PM <anamaria.valdez@gmail.com>
2023-05-01 10:51:59 -0700

    anav-pm-programming-argc-argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int i = argc - 1;
+
+	printf("%d\n", i);
+	return (0);
+	}
