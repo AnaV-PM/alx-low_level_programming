0880b2d194b1315ef5537f208d082c481b1538a6
AnaV-PM <anamaria.valdez@gmail.com>
2023-05-02 13:00:34 -0700

    anav-pm-programming-function-pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: pointer to the array
+ * @size: size of the array
+ * @action: pointer to a function that takes an int as argument
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (!array || !size || !action)
+		return;
+
+	while (size--)
+	action(*array++);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - simple aritmetic operation
+ * @arg: the number of arguments
+ * @arv: containing command line arguments
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(int arg, char *arv[])
+{
+	int n1, n2, result;
+	char op;
+	int (*f)(int, int);
+
+	if (arg != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	n1 = atoi(arv[1]);
+	n2 = atoi(arv[3]);
+	f = get_op_func(arv[2]);
+	if (f == NULL || arv[2][1] != 0)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	op = *arv[2];
+	if ((op == '/' || op == '%') && n2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	result = f(n1, n2);
+	printf("%d\n", result);
+	return (0);
+}
