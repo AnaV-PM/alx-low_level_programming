85bc012ab3a6f56a0a068720133df60032de22b2
AnaV-PM <anamaria.valdez@gmail.com>
2023-05-02 13:06:29 -0700

    anav-pm-programming-function-pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ *
+ * Return: 0  successfull
+ */
+int main(int argc, char **argv)
+{
+	char *a = (char *)main;
+	int b;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
+
+	while (b--)
+		printf("%02hhx%s", *a++, b ? " " : "\n");
+	return (0);
+}
