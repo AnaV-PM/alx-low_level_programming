58899b9a9061b5f27a0e1e8d47380f54d06571f3
AnaV-PM <anamaria.valdez@gmail.com>
2023-05-02 06:48:09 -0700

    anav-pm-programming-malloc-free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - Concatenates all the arguments of the program.
+ * @ac: Number of arguments passed to the program.
+ * @av: Array of arguments passed to the program.
+ * Return: Pointer to a new string, or NULL if it fails.
+ */
+anav *argstostr(int ac, anav **av)
+{
+	int i, j, k, size;
+	anav *arg;
+
+	size = 0;
+	k = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			size++;
+			j++;
+		}
+		size++;
+		i++;
+	}
+	arg = malloc((sizeof(anav) * size) + 1);
+	if (arg == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			arg[k] = av[i][j];
+			j++;
+			k++;
+		}
+		arg[k] = '\n';
+		k++;
+		i++;
+	}
+	arg[k] = '\0';
+	return (arg);
+}
