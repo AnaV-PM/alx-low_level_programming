89059c70a05e082fcc217804a81763bbb6e45d8a
AnaV-PM <anamaria.valdez@gmail.com>
2023-05-03 07:45:03 -0700

    anav-pm-programming-singlyLimkedList

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - Adds a new node at the beginning of a listint_t list
+ * @head:  pointer to a pointer to the head of the list
+ * @n: The integer value to store in the new node
+ *
+ * Return: The address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *now;
+	listint_t *new;
+
+	now = *head;
+	while (now && now->next != NULL)
+		now = now->next;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	if (now)
+		now->next = new;
+	else
+		*head = new;
+	return (new);
+}
