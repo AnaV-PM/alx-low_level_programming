3dce8ef5ef185611733a06a3bc1059e79d66de1c
AnaV-PM <anamaria.valdez@gmail.com>
2023-05-03 06:59:37 -0700

    anav-pm-programming-singlyLimkedList

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * listint_len - return the number of a listint_t list
+ * @h: linked list to the head of the list
+ *
+ * Return: The number of nodes in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
