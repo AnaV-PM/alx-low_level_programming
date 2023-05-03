37e2257c40cc6e4e448fc2ae264d7acb89f3aa11
AnaV-PM <anamaria.valdez@gmail.com>
2023-05-03 11:24:36 -0700

    anav-programming-singly-linked-lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * add_nodeint - Adds a new node at the beginning of a listint_t list
- * @head:  pointer to a pointer to the head of the list
- * @n: The integer value to store in the new node
- *
- * Return: The address of the new element, or NULL if it failed
+ * add_nodeint_end - add a new node at the end of a `listint_t` list
+ * @head: double pointer to head node
+ * @n: int value to store in new node
+ * Return: Address of new element or NULL if failed
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
