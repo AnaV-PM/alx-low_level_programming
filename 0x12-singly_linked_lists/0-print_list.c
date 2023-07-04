9f3e5c822ca4467df0fdc48684992c59b18a9f55
AnaV-PM <anamaria.valdez@gmail.com>
2023-07-04 06:24:44 -0700

    programming-singly-linked-list-anavpm

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,21 +2,28 @@
 #include <stdio.h>
 
 /**
- * print_listint - Prints all the elements of a listint_t list
+ * print_list - Prints all the elements of a listint_t list
  * @h: A pointer to the head of the list
  *
  * Return: The number of nodes in the list
  */
-size_t print_listint(const listint_t *h)
+
+size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		if (h->str == NULL)
+			printf("[%d] (nil)\n", 0);
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
 		count++;
+		h = h->next;
 	}
 
 	return (count);
 }
+
+
