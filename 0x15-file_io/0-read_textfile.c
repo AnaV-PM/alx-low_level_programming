232b5b7125ac44218ec7c3f40dccc2b68bdb4aee
AnaV-PM <anamaria.valdez@gmail.com>
2023-07-10 06:47:26 -0700

    programming-file_io-anavpm

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *fname;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	fname = malloc(sizeof(char) * (letters));
+	if (!fname)
+		return (0);
+
+	nrd = read(fd, fname, letters);
+	nwr = write(STDOUT_FILENO, fname, nrd);
+
+	close(fd);
+
+	free(fname);
+
+	return (nwr);
+	}
